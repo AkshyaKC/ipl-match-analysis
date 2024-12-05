a7f3ea29e394f057967908073c8a86ea73cc0f8e
Akshya Kumar KC <86335824+AkshyaKC@users.noreply.github.com>
2024-12-05 14:38:29 +0545

    Update match_info.sql

--- ipl match analysis/match_info.sql
+++ ipl match analysis/match_info.sql
@@ -1,94 +1,94 @@
 use ipl;
-select * from match_info;
+SELECT * FROM match_info;
 
 -- Checking if any ipl match has been played outside india --
-select season_year,country_name, count(country_name) as Matches 
-from match_info
-where country_name != 'India'
-group by season_year, country_name;
+SELECT season_year,country_name, COUNT(country_name) AS Matches 
+FROM match_info
+WHERE country_name != 'India'
+GROUP BY season_year, country_name;
 
 -- Most matches played venue --
-select venue_name, city_name, country_name, count(venue_name) as Count
-from match_info
-group by venue_name, city_name, country_name
-order by Count desc;
+SELECT venue_name, city_name, country_name, COUNT(venue_name) AS Count
+FROM match_info
+GROUP BY venue_name, city_name, country_name
+ORDER BY Count DESC ;
 
 -- Changing different labeling for Bangalore to Bangalore --
-select venue_name, 
-case
-when city_name in ('Bangalore', 'Bengaluru') then 'Bangalore'
-else city_name
-end as city_name2, 
-country_name, count(venue_name) as Count 
-from match_info
-group by venue_name, city_name2, country_name
-order by Count desc;
+SELECT venue_name, 
+CASE
+	WHEN city_name IN ('Bangalore', 'Bengaluru') THEN 'Bangalore'
+	ELSE city_name
+END AS city_name2, 
+country_name, COUNT(venue_name) AS Count 
+FROM match_info
+GROUP BY venue_name, city_name2, country_name
+ORDER BY Count DESC ;
 
 -- Is there any difference between number of matches thoroughout the different season ?--
-select season_year, count(season_year) as Count
-from match_info
-group by season_year
-order by  Count desc; 
+SELECT season_year, COUNT(season_year) AS Count
+FROM match_info
+GROUP BY season_year
+ORDER BY  Count DESC ; 
 
 -- Let's see which team has won the toss most.--
-select toss_winner, count(toss_winner) as Count
-from match_info
-group by toss_winner
-order by Count desc;
+SELECT toss_winner, COUNT(toss_winner) AS Count
+FROM match_info
+GROUP BY toss_winner
+ORDER BY Count DESC ;
 
 /* Since the chances of winning toss increase as the number of games played by a team increase. Mumbai Indians and KKR
 being at the top of the list may be because of they qualified for the knockout games more. Let's check if this is true */
 
-with team_cte as(
-	select team1 as team_played
-	from match_info
-	union all
-	select team2 as team_played
-	from match_info
+WITH team_cte AS(
+	SELECT team1 AS team_played
+	FROM match_info
+	UNION ALL
+	SELECT team2 AS team_played
+	FROM match_info
 )
-select team_played, count(team_played) as Count
-from team_cte
-group by team_played
-order by Count desc;
+SELECT team_played, COUNT(team_played) AS Count
+FROM team_cte
+GROUP BY team_played
+ORDER BY Count DESC ;
 
 -- ORRRRRRRRR --
  
-select team_played, count(team_played) as team_count
-from (
+SELECT team_played, count(team_played) as team_count
+FROM (
     SELECT team1 AS team_played
     FROM match_info
     UNION ALL
     SELECT team2 AS team_played
     FROM match_info
 ) AS combined_count
-group by team_played
-order by team_count desc;
+GROUP BY team_played
+ORDER BY team_count DESC ;
 
 
 -- Let's merge these two tables to calculate the toss win percentage for each team. -- 
-with play_count as (
-	select team_played, count(team_played) as play_count
-	from (
+WITH play_count AS (
+	SELECT team_played, count(team_played) AS play_count
+	FROM (
 		SELECT team1 AS team_played
 		FROM match_info
 		UNION ALL
 		SELECT team2 AS team_played
 		FROM match_info
 	) AS combined_count
-	group by team_played),
-toss_win_count as (
-	select toss_winner, count(toss_winner) as toss_win_count
-	from match_info
-	group by toss_winner)
-select team_played, play_count, toss_win_count, round((toss_win_count / play_count)*100,2) as toss_win_percentage
-from play_count as pc
-left join toss_win_count as twc on twc.toss_winner=pc.team_played
-order by toss_win_percentage desc;
+	GROUP BY team_played),
+toss_win_count AS (
+	SELECT toss_winner, COUNT(toss_winner) AS toss_win_count
+	FROM match_info
+	GROUP BY toss_winner)
+SELECT team_played, play_count, toss_win_count, round((toss_win_count / play_count)*100,2) as toss_win_percentage
+FROM play_count AS pc
+LEFT JOIN toss_win_count AS twc ON twc.toss_winner=pc.team_played
+ORDER BY toss_win_percentage DESC ;
 
 -- Analyzing the most preferred choice for an IPL captain after winning the toss. --
-select toss_name, count(toss_name) as Count
-from match_info
-group by toss_name;
+SELECT toss_name, count(toss_name) as Count
+FROM match_info
+GROUP BY toss_name;
 
 -- What are the chances of winning match after winning the toss ? --
          -- Overall win percentage after winning the toss --
@@ -101,33 +101,33 @@ FROM
     (SELECT COUNT(*) AS Match_Win FROM match_info WHERE toss_winner = match_winner) AS match_wins;
 
 -- Team's win percentage after winning the toss --
-with toss_win as(
-	select toss_winner, count(*) as Toss_Win
-    from match_info
-    group by toss_winner ),
+WITH toss_win AS(
+	SELECT toss_winner, COUNT(*) AS Toss_Win
+    FROM match_info
+    GROUP BY toss_winner ),
     toss_match_win as(
-	select match_winner, count(*) as Toss_Match_Win
-	from match_info
-	where toss_winner = match_winner
-	group by match_winner)
-select toss_winner, 
+	SELECT match_winner, count(*) AS Toss_Match_Win
+	FROM match_info
+	WHERE toss_winner = match_winner
+	GROUP BY match_winner)
+SELECT toss_winner, 
 Toss_Win, Toss_Match_Win, 
-round((Toss_Match_Win/Toss_Win)*100, 2) as Percentage
-from toss_win as tw
-join toss_match_win as tmw on tmw.match_winner=tw.toss_winner
-order by Percentage desc;
+round((Toss_Match_Win/Toss_Win)*100, 2) AS Percentage
+FROM toss_win AS tw
+join toss_match_win AS tmw ON tmw.match_winner=tw.toss_winner
+ORDER BY Percentage DESC ;
 
 -- Let's examine the result type when a match is scheduled --
-select outcome_type, count(outcome_type) as Count
-from match_info
-group by outcome_type
-order by Count desc;
+SELECT outcome_type, COUNT(outcome_type) as Count
+FROM match_info
+GROUP BY outcome_type
+ORDER BY Count DESC ;
 
 -- Most man of the match --
-select manofmach, count(manofmach) as Count
-from match_info
-group by manofmach
-order by Count desc;
+SELECT manofmach, COUNT(manofmach) AS Count
+FROM match_info
+GROUP BY manofmach
+ORDER BY Count DESC ;
 
-/* The man of the match is most probably given to the players from winning team. 
-For this we'll need to select the manofmatch from match_info table and join the result Let's see if this is true or not */
+/* The man of the match is most probably given to the players FROM winning team. 
+For this we'll need to SELECT the manofmatch FROM match_info table and join the result Let's see if this is true or not */
